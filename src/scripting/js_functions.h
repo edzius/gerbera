c48380b1637d52436a899df3501f7a506fc05fe3
Edvinas Stunžėnas <edvinas.stunzenas@gmail.com>
2019-03-19 01:45:39 +0200

    scripts: expose HTTP GET function into JS API
    This method should help to retrieve media meta
    information from external sources. Usefull for
    those whom network latency is not a big problem.
    Call exposed as `doHttpGet(url)` global function
    in JS script. Returned output is either string
    if data retrieved succesfully or `undefined` on
    failure.

--- src/scripting/js_functions.h
+++ src/scripting/js_functions.h
@@ -46,6 +46,11 @@ duk_ret_t js_addCdsObject(duk_context *ctx);
 /// \brief Makes a copy of an CDS object.
 duk_ret_t js_copyObject(duk_context *ctx);
 
+#ifdef HAVE_CURL
+/// \brief Perform HTTP GET request and return response.
+duk_ret_t js_doHttpGet(duk_context *ctx);
+#endif
+
 /// filesystem charset to internal
 duk_ret_t js_f2i(duk_context *ctx);
 /// metadata charset to internal
